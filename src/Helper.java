63e99d440a79aee16c6241b605a919e3bd70fa00
ombrexxx <martinkovic.ondrej@gmail.com>
2020-06-06 10:25:46 +0200

    third commit

---
+++ src/Helper.java
@@ -0,0 +1,9 @@
+public class Helper {
+
+    public static String gettingString(String something) {
+        something += "laaaa";
+        something += "blaaaa";
+        return something;
+    }
+
+}
